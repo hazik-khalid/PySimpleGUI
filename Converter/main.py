129a5b4edac1bed59eec80a6cba8ea5fd239a788
hazik-khalid <hazikkhalid111@google.com>
2024-02-21 15:51:39 +0530

    bug-fix

--- Converter/main.py
+++ Converter/main.py
@@ -1,21 +1,22 @@
 import PySimpleGUI as sg
 
-layout =[
-    [sg.Text('converter' , enable_events=True) , sg.Spin(['name1','name2'])],
-    [sg.Input(key='input')],
-    [sg.Button('text')]
+layout = [
+    [sg.Input(key="-input-"),
+      sg.Spin(['km to mile', 
+               'kg to pound','sec to min'],key='-units-'), sg.Button('convert' ,key='-convert-')],
+    [sg.Text('output', key='-output-')]
         ]
 
-window =sg.Window('converter',layout)
+window = sg.Window("converter", layout)
 
 while True:
-    event,values=window.read()
-      
-    if event== sg.WIN_CLOSED:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
         break
+    if event=='-convert-':
+        input_value=values["-input-"]
+        print(input_value)
 
- 
 
-  
-        
 window.close()
